05ea69805c54b31511d03d1e1e436b9467b6434f
cesseti <carlosribeiro.cr146@gmail.com>
2026-08-14 10:40:15 -0300

    feat: Criou a interface MissaoRepository e extendeu o JPA

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missao/MissaoRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Missao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
