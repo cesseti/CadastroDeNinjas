e8fd97dc0564c2acefedb46ec6ccae23d5baa64e
cesseti <carlosribeiro.cr146@gmail.com>
2026-09-10 00:22:02 -0300

    fix: Tira a responsabilidade (para os ninjas) do model e passa para o DTO usando streams no mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class NinjaService {
@@ -22,18 +23,25 @@ public class NinjaService {
         return ninjaMapper.map(ninja);
     }
 
-    public List<NinjaModel> listarNinjas() {
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas() {
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
     }
 
-    public NinjaModel buscarPorId(Long id) {
-        Optional<NinjaModel> ninjaModel = ninjaRepository.findById(id);
-        return ninjaModel.orElse(null);
+    public NinjaDTO buscarPorId(Long id) {
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
 
-    public NinjaModel editar(Long id, NinjaModel ninjaEditado) {
-        if (ninjaRepository.existsById(id)) {
-            return ninjaRepository.save(ninjaEditado);
+    public NinjaDTO editar(Long id, NinjaDTO ninjaDTO) {
+        Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
+        if(ninjaExistente.isPresent()){
+            NinjaModel ninjaEditado = ninjaMapper.map(ninjaDTO);
+            ninjaEditado.setId(id);
+            NinjaModel ninjaSalvo = ninjaRepository.save(ninjaEditado);
+            return ninjaMapper.map(ninjaSalvo);
         }
         return null;
     }
